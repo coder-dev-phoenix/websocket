cdbf687413ec46d789b0cbab1453728b98572cbc
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2020-02-26 01:32:48 +0500

    add feature message is sent to all sockets

--- index.js
+++ index.js
@@ -11,5 +11,13 @@ app.use(express.static("public"));
 const io = socket(server);
 
 io.on("connection", function(socket) {
-	console.log("Successfully connected to client");
+	console.log("Successfully connected to client", socket.id);
+
+	// checking the client did the sent something if yes then it will sent to
+	// all socket means client
+	socket.on("chat", function(data) {
+        console.log("server side");
+		io.emit("chat", data);
+	});
 });
+
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,25 @@
+let socket = io.connect('http://localhost:4000');
+
+let message = document.getElementById("message");
+let handle = document.getElementById("handle");
+let btn = document.getElementById("send");
+let output = document.getElementById("output");
+console.log(message, handle, btn, output);
+
+// Event emitting sending to the server
+btn.addEventListener("click", function() {
+	console.log("client side");
+	socket.emit("chat", {
+		message: message.value,
+		handle: handle.value
+    });
+    message.value = "";
+});
+
+// Listen to the server did the sent something
+socket.on("chat", function(data) {
+	output.innerHTML +=
+		"<p><strong>" + data.handle + ":</strong> " + data.message + "</p>";
+});
 
 
-var socket = io.connect("http://localhost:4000/");
